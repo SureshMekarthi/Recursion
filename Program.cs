907f1bd2bc60052e2c92f4b2909c1e0b5ebbc322
AnushaPoosala <anusha.poosala@outlook.com>
2024-02-25 11:28:35 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintNumber(5);
+           Console.ReadLine();
+        }
+
+        private static void PrintNumber(int i)
+        {
+            if (i > 0)
+            {
+                Console.WriteLine($"the i value is: {i}");
+                PrintNumber(i-1);
+            }
+            return;
+        }
+    }
+}
